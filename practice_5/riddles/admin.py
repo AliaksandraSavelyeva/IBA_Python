077bf1a189bbacb98f17e2f283a3f1e4abe273fa
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2021-06-17 05:49:28 +0300

    db migration + html

---
+++ practice_5/riddles/admin.py
@@ -0,0 +1,8 @@
+# savelyeva_a
+# 123456
+
+from django.contrib import admin
+from .models import Option, Riddle
+
+admin.site.register(Riddle)
+admin.site.register(Option)
--- practice_5/riddles/urls.py
+++ practice_5/riddles/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'riddles'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^([0-9]+)/$', views.detail, name='detail'),
+    url(r'^([0-9]+)/answer/$', views.answer, name='answer')
 ]
